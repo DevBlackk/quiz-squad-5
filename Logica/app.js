562249186a983a99345f3caf56b7f009c784cbe4
Sebastião Cosme Agostinho <81954475+scosme20@users.noreply.github.com>
2023-08-03 08:59:06 -0300

    Add files via upload

---
+++ Logica/app.js
@@ -0,0 +1,42 @@
+var acerto = 0;
+var perguntaUm = " "
+
+function iniciar() {
+    var nome = prompt("Digite seu nome:")
+}
+
+
+function primeira () {
+    if(prompt("insira a resposta da pergunta 1") =="a") {
+        alert("Voce acertou")
+    } else {
+        alert("Tente novamente")
+    }
+}
+
+
+
+function segunda () {
+    if(prompt("insira a resposta da pergunta 1") =="c") {
+        alert("Voce acertou")
+    } else {
+        alert("Tente novamente")
+    }
+    }
+
+function terceira () {
+    if(prompt("insira a resposta da pergunta 1") =="d") {
+        alert("Voce acertou")
+    } else {
+        alert("Tente novamente")
+    }
+    }
+
+
+function quarta () {
+    if(prompt("insira a resposta da pergunta 1") =="b") {
+        alert("Voce acertou")
+    } else {
+        alert("Tente novamente")
+    }
+    }
